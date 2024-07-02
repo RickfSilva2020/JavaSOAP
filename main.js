8dcd87c4d03582debb271e7fb95c996a18784465
Ricardo Silva <109364648+RickfSilva2020@users.noreply.github.com>
2024-07-02 13:30:17 -0300

    Version 1.01

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
 
@@ -27,33 +26,3 @@ app.whenReady().then(() => {
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') app.quit();
 });
-=======
-const { app, BrowserWindow } = require('electron');
-const path = require('path');
-
-function createWindow() {
-    const win = new BrowserWindow({
-        width: 800,
-        height: 600,
-        webPreferences: {
-            preload: path.join(__dirname, 'renderer.js'),
-            nodeIntegration: true,
-            contextIsolation: false
-        }
-    });
-
-    win.loadFile('index.html');
-}
-
-app.whenReady().then(() => {
-    createWindow();
-
-    app.on('activate', () => {
-        if (BrowserWindow.getAllWindows().length === 0) createWindow();
-    });
-});
-
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') app.quit();
-});
->>>>>>> f7648bdeda7e864bb320c4a5fd5f76608463abdd
